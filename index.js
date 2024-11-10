83dce21d27ce904d04dce1a5327247b687749854
Kyan Oberas <kyanmigueloberas1@gmail.com>
2024-11-10 00:25:48 -0500

    connected to Cosmos DB

--- index.js
+++ index.js
@@ -8,15 +8,10 @@ require('dotenv').config();
 const app = express();
 const PORT = 5000;
 
-// Connect to Azure MongoDB database
-const mongoURI = process.env.MONGO_URI;
+mongoose.connect(process.env.MONGO_URI) // mongodb://localhost:27017/testdb
 
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("Failed to connect to MongoDB", err));
+.then(() => console.log("Connected to Azure Cosmos DB"))
+.catch(err => console.error("Failed to connect to Azure Cosmos DB", err));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
