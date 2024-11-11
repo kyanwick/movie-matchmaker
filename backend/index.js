33a0168c32b750b937497d074eddb641fc410a3e
Kyan Oberas <kyanmigueloberas1@gmail.com>
2024-11-10 20:37:42 -0500

    static files in backend

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 // Enable CORS to allow requests from frontend
